f7b181aa5516d104c19d74e8a0a7759ef01bd33e
marsya505 <marsyazulfazahrani0@gmail.com>
2026-06-24 10:57:32 +0700

    update login logout

--- backends/logout.php
+++ backends/logout.php
@@ -1,6 +1,13 @@
 <?php
 
 session_start();
+require 'koneksi/koneksi.php';
+require 'koneksi/activity_logger.php';
+
+/* catat logout SEBELUM session dihapus */
+if (isset($_SESSION['user_id'])) {
+    log_activity($conn, $_SESSION['user_id'], 'logout', 'auth', "User {$_SESSION['fullname']} logout");
+}
 
 /* hapus semua session */
 session_unset();
